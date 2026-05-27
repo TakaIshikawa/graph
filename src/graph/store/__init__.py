0e55af266a993d327274e42fba34387df1f51ce4
TakaIshikawa <hiro52ishikawa.us@gmail.com>
2026-05-27 16:52:14 +0900

    Relay batch integration: Add Unit Word Count CSV Export

--- src/graph/store/__init__.py
+++ src/graph/store/__init__.py
@@ -10,22 +10,29 @@ from graph.store.unit_blockquote_usage_summary import summarize_unit_blockquote_
 from graph.store.unit_callout_usage_summary import summarize_unit_callout_usage
 from graph.store.unit_code_fence_info_attribute_summary import summarize_unit_code_fence_info_attributes
 from graph.store.unit_content_encoding_issue_summary import summarize_unit_content_encoding_issues
+from graph.store.unit_bare_url_summary import summarize_unit_bare_urls
 from graph.store.unit_duplicate_content_hash_summary import summarize_unit_duplicate_content_hashes
+from graph.store.unit_duplicate_title_summary import summarize_unit_duplicate_titles
 from graph.store.unit_duplicate_external_id_summary import summarize_unit_duplicate_external_ids
 from graph.store.unit_empty_content_summary import summarize_unit_empty_content
 from graph.store.unit_external_url_domain_summary import summarize_unit_external_url_domains
 from graph.store.unit_footnote_orphan_summary import summarize_unit_footnote_orphans
 from graph.store.unit_frontmatter_array_field_summary import summarize_unit_frontmatter_array_fields
+from graph.store.unit_frontmatter_multiline_field_summary import summarize_unit_frontmatter_multiline_fields
+from graph.store.unit_frontmatter_required_key_summary import summarize_unit_frontmatter_required_keys
 from graph.store.unit_heading_hierarchy_summary import summarize_unit_heading_hierarchy
 from graph.store.unit_html_heading_anchor_summary import summarize_unit_html_heading_anchors
+from graph.store.unit_html_tag_usage_summary import summarize_unit_html_tag_usage
 from graph.store.unit_inline_code_usage_summary import summarize_unit_inline_code_usage
 from graph.store.unit_local_file_reference_summary import summarize_unit_local_file_references
 from graph.store.unit_markdown_abbreviation_summary import summarize_unit_markdown_abbreviations
+from graph.store.unit_markdown_autolink_summary import summarize_unit_markdown_autolinks
 from graph.store.unit_markdown_custom_id_summary import summarize_unit_markdown_custom_ids
 from graph.store.unit_markdown_escape_summary import summarize_unit_markdown_escapes
 from graph.store.unit_markdown_hard_break_summary import summarize_unit_markdown_hard_breaks
 from graph.store.unit_markdown_highlight_summary import summarize_unit_markdown_highlights
 from graph.store.unit_markdown_horizontal_rule_summary import summarize_unit_markdown_horizontal_rules
+from graph.store.unit_markdown_table_alignment_summary import summarize_unit_markdown_table_alignments
 from graph.store.unit_markdown_strikethrough_summary import summarize_unit_markdown_strikethrough
 from graph.store.unit_markdown_task_priority_summary import summarize_unit_markdown_task_priorities
 from graph.store.unit_markdown_math_summary import summarize_unit_markdown_math
@@ -37,6 +44,8 @@ from graph.store.unit_yaml_alias_anchor_summary import summarize_unit_yaml_alias
 from graph.store.unit_markdown_definition_list_summary import summarize_unit_markdown_definition_lists
 from graph.store.unit_markdown_comment_directive_summary import summarize_unit_markdown_comment_directives
 from graph.store.unit_metadata_secret_hint_summary import summarize_unit_metadata_secret_hints
+from graph.store.unit_notebook_cell_marker_summary import summarize_unit_notebook_cell_markers
+from graph.store.unit_pandoc_citation_key_summary import summarize_unit_pandoc_citation_keys
 from graph.store.unit_tag_cardinality_summary import summarize_unit_tag_cardinality
 from graph.store.unit_tag_prefix_summary import summarize_unit_tag_prefixes
 from graph.store.unit_word_count_distribution_summary import summarize_unit_word_count_distribution
@@ -55,26 +64,35 @@ __all__ = [
     "summarize_unit_callout_usage",
     "summarize_unit_code_fence_info_attributes",
     "summarize_unit_content_encoding_issues",
+    "summarize_unit_bare_urls",
     "summarize_unit_duplicate_content_hashes",
+    "summarize_unit_duplicate_titles",
     "summarize_unit_duplicate_external_ids",
     "summarize_unit_empty_content",
     "summarize_unit_external_url_domains",
     "summarize_unit_footnote_orphans",
     "summarize_unit_frontmatter_array_fields",
+    "summarize_unit_frontmatter_multiline_fields",
+    "summarize_unit_frontmatter_required_keys",
     "summarize_unit_heading_hierarchy",
     "summarize_unit_html_heading_anchors",
+    "summarize_unit_html_tag_usage",
     "summarize_unit_inline_code_usage",
     "summarize_unit_local_file_references",
     "summarize_unit_markdown_abbreviations",
+    "summarize_unit_markdown_autolinks",
     "summarize_unit_markdown_custom_ids",
     "summarize_unit_markdown_escapes",
     "summarize_unit_markdown_hard_breaks",
     "summarize_unit_markdown_highlights",
     "summarize_unit_markdown_horizontal_rules",
+    "summarize_unit_markdown_table_alignments",
     "summarize_unit_markdown_strikethrough",
     "summarize_unit_markdown_task_priorities",
     "summarize_unit_markdown_comment_directives",
     "summarize_unit_markdown_definition_lists",
+    "summarize_unit_notebook_cell_markers",
+    "summarize_unit_pandoc_citation_keys",
     "summarize_unit_yaml_alias_anchors",
     "summarize_unit_math_notation",
     "summarize_unit_doi_hints",
