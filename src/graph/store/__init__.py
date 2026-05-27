6175676f05865dd4bc44efd68f3b1c9ef224de7e
TakaIshikawa <hiro52ishikawa.us@gmail.com>
2026-05-27 13:05:40 +0900

    Relay batch integration: Add Store Unit Word Count Distribution Summary

--- src/graph/store/__init__.py
+++ src/graph/store/__init__.py
@@ -1,17 +1,23 @@
 from graph.store.backup import export_store_backup
-from graph.store.unit_callout_usage_summary import summarize_unit_callout_usage
+from graph.store.collection_metadata_drift_summary import summarize_collection_metadata_drift
+from graph.store.relation_metadata_key_frequency_summary import summarize_relation_metadata_key_frequency
 from graph.store.unit_attachment_extension_summary import summarize_unit_attachment_extensions
+from graph.store.unit_callout_usage_summary import summarize_unit_callout_usage
 from graph.store.unit_duplicate_content_hash_summary import summarize_unit_duplicate_content_hashes
 from graph.store.unit_empty_content_summary import summarize_unit_empty_content
 from graph.store.unit_external_url_domain_summary import summarize_unit_external_url_domains
 from graph.store.unit_tag_cardinality_summary import summarize_unit_tag_cardinality
+from graph.store.unit_word_count_distribution_summary import summarize_unit_word_count_distribution
 
 __all__ = [
     "export_store_backup",
+    "summarize_collection_metadata_drift",
+    "summarize_relation_metadata_key_frequency",
     "summarize_unit_attachment_extensions",
     "summarize_unit_callout_usage",
     "summarize_unit_duplicate_content_hashes",
     "summarize_unit_empty_content",
     "summarize_unit_external_url_domains",
     "summarize_unit_tag_cardinality",
+    "summarize_unit_word_count_distribution",
 ]
