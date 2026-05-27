6caabe74cfc408828918eeef34f4ff19eb1a3f4e
TakaIshikawa <hiro52ishikawa.us@gmail.com>
2026-05-27 13:09:26 +0900

    Relay batch integration: Add Store Source Ingest Frequency Summary

--- src/graph/store/__init__.py
+++ src/graph/store/__init__.py
@@ -1,6 +1,8 @@
 from graph.store.backup import export_store_backup
+from graph.store.source_ingest_frequency_summary import summarize_source_ingest_frequency
 from graph.store.unit_callout_usage_summary import summarize_unit_callout_usage
 from graph.store.unit_attachment_extension_summary import summarize_unit_attachment_extensions
+from graph.store.unit_content_encoding_issue_summary import summarize_unit_content_encoding_issues
 from graph.store.unit_duplicate_content_hash_summary import summarize_unit_duplicate_content_hashes
 from graph.store.unit_empty_content_summary import summarize_unit_empty_content
 from graph.store.unit_external_url_domain_summary import summarize_unit_external_url_domains
@@ -8,8 +10,10 @@ from graph.store.unit_tag_cardinality_summary import summarize_unit_tag_cardinal
 
 __all__ = [
     "export_store_backup",
+    "summarize_source_ingest_frequency",
     "summarize_unit_attachment_extensions",
     "summarize_unit_callout_usage",
+    "summarize_unit_content_encoding_issues",
     "summarize_unit_duplicate_content_hashes",
     "summarize_unit_empty_content",
     "summarize_unit_external_url_domains",
