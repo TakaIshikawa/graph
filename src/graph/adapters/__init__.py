0d4fa2932f5ee8ecd57f39e077e8e042c9786b8c
TakaIshikawa <hiro52ishikawa.us@gmail.com>
2026-05-31 12:41:24 +0900

    Relay batch integration: Add Unit Markdown Image Alt Text Inventory CSV Export

--- src/graph/adapters/__init__.py
+++ src/graph/adapters/__init__.py
@@ -1,6 +1,8 @@
 from graph.adapters.google_keep_takeout_json import GoogleKeepTakeoutJsonAdapter
 from graph.adapters.letterboxd_diary_csv import LetterboxdDiaryCsvAdapter
 from graph.adapters.mastodon_bookmarks_json import MastodonBookmarksJsonAdapter
+from graph.adapters.apple_notes_takeout_html import AppleNotesTakeoutHtmlAdapter
+from graph.adapters.omnivore_library_json import OmnivoreLibraryJsonAdapter
 from graph.adapters.pinboard_bookmarks_csv import PinboardBookmarksCsvAdapter
 from graph.adapters.pocket_bookmarks_csv import PocketBookmarksCsvAdapter
 from graph.adapters.raindrop_bookmarks_csv import RaindropBookmarksCsvAdapter
@@ -11,6 +13,8 @@ __all__ = [
     "GoogleKeepTakeoutJsonAdapter",
     "LetterboxdDiaryCsvAdapter",
     "MastodonBookmarksJsonAdapter",
+    "AppleNotesTakeoutHtmlAdapter",
+    "OmnivoreLibraryJsonAdapter",
     "PinboardBookmarksCsvAdapter",
     "PocketBookmarksCsvAdapter",
     "RaindropBookmarksCsvAdapter",
