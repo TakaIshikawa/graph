645c552b9a39205657be698f327d36da6b6a9656
TakaIshikawa <hiro52ishikawa.us@gmail.com>
2026-06-02 22:59:03 +0900

    Relay batch integration: Add Store Source Accept-CH Summary

--- src/graph/adapters/__init__.py
+++ src/graph/adapters/__init__.py
@@ -11,6 +11,11 @@ from graph.adapters.pocket_bookmarks_csv import PocketBookmarksCsvAdapter
 from graph.adapters.raindrop_bookmarks_csv import RaindropBookmarksCsvAdapter
 from graph.adapters.steam_playtime_csv import SteamPlaytimeCsvAdapter
 from graph.adapters.zotero_items_csv import ZoteroItemsCsvAdapter
+from graph.adapters.github_starred_repos_json import GitHubStarredReposJsonAdapter
+from graph.adapters.hackernews_favorites_csv import HackerNewsFavoritesCsvAdapter
+from graph.adapters.goodreads_books_csv import GoodreadsBooksCsvAdapter
+from graph.adapters.rss_subscriptions_opml import RssSubscriptionsOpmlAdapter
+from graph.adapters.browser_history_csv import BrowserHistoryCsvAdapter
 
 __all__ = [
     "GoogleKeepTakeoutJsonAdapter",
@@ -26,4 +31,9 @@ __all__ = [
     "RaindropBookmarksCsvAdapter",
     "SteamPlaytimeCsvAdapter",
     "ZoteroItemsCsvAdapter",
+    "GitHubStarredReposJsonAdapter",
+    "HackerNewsFavoritesCsvAdapter",
+    "GoodreadsBooksCsvAdapter",
+    "RssSubscriptionsOpmlAdapter",
+    "BrowserHistoryCsvAdapter",
 ]
